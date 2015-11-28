d6345409e4067ac37d31baefaf1b2c1799ab3150
Yuriy Tymochko <rain.yura@gmail.com>
2015-11-28 14:50:28 +0200

    Added homework10 for inspection

---
+++ GoIT/markup/home/lesson_10-11/js/script10.js
@@ -0,0 +1,14 @@
+var base = prompt("Enter your number", '');
+var exponent = prompt("Enter degree for your number", '');
+
+function pow(base, exponent) {
+  var result = 1;
+  for (var k = 0; k < exponent; k++) {
+    result *= base;
+  }
+  
+  return result;
+}
+
+var calculationResult = pow(base, exponent);
+console.log('Result is ', calculationResult);
