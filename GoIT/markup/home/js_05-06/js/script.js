72b3ab0f334e6cf5fdcd5cb5c408f51053bb9204
Yuriy Tymochko <rain.yura@gmail.com>
2015-12-27 14:09:57 +0200

    Modified timeChange function

--- GoIT/markup/home/js_05-06/js/script.js
+++ GoIT/markup/home/js_05-06/js/script.js
@@ -26,64 +26,71 @@ miliseconds.textContent = '000';
 seconds.textContent = '00';
 minutes.textContent = '00';
 hours.textContent = '00';
-//
-//miliseconds = +(miliseconds.textContent);
-//seconds = +(seconds.textContent);
-//minutes = +(minutes.textContent);
-//hours = +(hours.textContent);
+
+miliseconds = +(miliseconds.textContent);
+seconds = +(seconds.textContent);
+minutes = +(minutes.textContent);
+hours = +(hours.textContent);
+
+miliseconds = 0;
 
 function timeChange() {
 
-  for (var ms = 0; ms < 1000; ms++) {
-    if (ms < 10) {
-      miliseconds.textContent = '00' + ms;
-    } else if (ms >= 100) {
-      miliseconds.textContent = ms;
-    } else {
-      miliseconds.textContent = '0' + ms;
-    };
-  };
+  miliseconds++;
+  miliseconds.textContent = miliseconds.toString();
 
-  miliseconds.textContent = '000';
-
-  for (var s = 0; s < 60; s++) {
-    if (s < 10) {
-      seconds.textContent = '0' + s;
-    } else {
-      seconds.textContent = s;
-    };
-  };
-
-  seconds.textContent = '00';
-
-  for (var m = 0; m < 60; m++) {
-    if (m < 10) {
-      minutes.textContent = '0' + m;
-    } else {
-      minutes.textContent = m;
-    };
-  };
-
-  minutes.textContent = '00';
-
-  for (var h = 0; h < 24; h++) {
-    if (h < 10) {
-      hours.textContent = '0' + h;
-    } else {
-      hours.textContent = h;
-    };
-  };
-
-  hours.textContent = '00';
 };
+//  for (var ms = 0; ms < 1000; ms++) {
+//    if (ms < 10) {
+//      miliseconds.textContent = '00' + ms;
+//    } else if (ms >= 100) {
+//      miliseconds.textContent = ms;
+//    } else {
+//      miliseconds.textContent = '0' + ms;
+//    }
+//    ;
+//  };
+//
+//  miliseconds.textContent = '000';
+//
+//  for (var s = 0; s < 60; s++) {
+//    if (s < 10) {
+//      seconds.textContent = '0' + s;
+//    } else {
+//      seconds.textContent = s;
+//    };
+//  };
+//
+//  seconds.textContent = '00';
+//
+//  for (var m = 0; m < 60; m++) {
+//    if (m < 10) {
+//      minutes.textContent = '0' + m;
+//    } else {
+//      minutes.textContent = m;
+//    };
+//  };
+//
+//  minutes.textContent = '00';
+//
+//  for (var h = 0; h < 24; h++) {
+//    if (h < 10) {
+//      hours.textContent = '0' + h;
+//    } else {
+//      hours.textContent = h;
+//    };
+//  };
+//
+//  hours.textContent = '00';
+//};
 
-function pauseTimer() {
-  buttonStart.addEventListener(switchButtonStart);
-  clearInterval(timerMiliseconds);
-}
-
-var buttonPause = buttonStart;
-buttonPause.addEventListener('click', pauseTimer);
+//function pauseTimer() {
+//  buttonStart.removeEventListener(switchButtonPause);
+//  clearInterval(timerMiliseconds);
+//}
+//
+//var buttonPause = document.getElementById('button-start');
+//buttonPause.addEventListener('click', pauseTimer);
 
 function clearTimer() {
   buttonStart.removeEventListener(switchButtonPause);
